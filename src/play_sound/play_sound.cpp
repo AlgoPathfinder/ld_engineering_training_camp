2a701a9cb7e292768eea8459d74d092d4c2a761d
AlgoPathfinder <Ang.L@qq.com>
2023-08-18 00:26:41 +0800

    change sound file names

--- src/play_sound/play_sound.cpp
+++ src/play_sound/play_sound.cpp
@@ -4,12 +4,15 @@ int main()
 {
 	int ret = 0;
 
-	ret |= system("play ../sound/front.mp3");
-	ret |= system("play ../sound/left.mp3");
-	ret |= system("play ../sound/right.mp3");
-	ret |= system("play ../sound/leftfront.mp3");
-	ret |= system("play ../sound/rightfront.mp3");
-
+	ret |= system("play ../sound/front.wav");
+	ret |= system("play ../sound/left.wav");
+	ret |= system("play ../sound/right.wav");
+	ret |= system("play ../sound/leftfront.wav");
+	ret |= system("play ../sound/rightfront.wav");
+	ret |= system("play ../sound/back.wav");
+	ret |= system("play ../sound/failed.wav");
+	ret |= system("play ../sound/finished.wav");
+	
 	std::cout << "ret=" << ret << "	Test end!" << std::endl;
 	return 0;
 }
